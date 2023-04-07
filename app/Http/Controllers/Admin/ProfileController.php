ee60f4db4097fdddebd8d0d214d67be1e7bce6f4
近藤亜美 <ami.m.m.1227@gmail.com>
2023-04-07 13:06:55 +0000

    PHP/Laravel 12

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function add()
     {
         return view('admin.profile.create');
